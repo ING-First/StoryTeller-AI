40ac05b1b3667c939a6b2b2d2cd605085e71fa32
songwonjoon <djckoreas@naver.com>
2025-08-17 23:26:36 +0900

    Feat: delete-user api

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 
 from typing import Union, Optional
 from fastapi import FastAPI, Depends, HTTPException, Query
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordBearer
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from db import SessionLocal
@@ -9,7 +9,7 @@ from db_models import Users, FairyTale, FairyTaleLog
 from generate_summary import Summarizer
 from datetime import date, datetime, timedelta
 from passlib.context import CryptContext
-from jose import JWTError, jwt
+from jose import jwt
 from dotenv import load_dotenv
 import os
 import re
@@ -107,6 +107,13 @@ class UserUpdateRequest(BaseModel):
 class UserUpdateResponse(BaseModel):
     message: str
 
+class UserDeleteRequest(BaseModel):
+    uid: int
+    passwd: str
+
+class UserDeleteResponse(BaseModel):
+    message: str
+
 # 회원가입 API
 @app.post("/join", response_model=UserResponse)
 def join(req: UserRequest, db: Session = Depends(get_db)):
@@ -182,7 +189,7 @@ def login(req: LoginRequest, db: Session = Depends(get_db)):
     if req.passwd == "":
         raise HTTPException(status_code=400, detail="비밀번호를 입력해주세요.")
     
-    user = db.query(Users).filter(Users.id == req.id).first()
+    user = db.query(Users).filter(Users.id == req.id, Users.useFlag == 1).first()
     if not user or not verify_password(req.passwd, user.passwd):
         raise HTTPException(status_code=401, detail="아이디 또는 비밀번호가 잘못되었습니다.")
     
@@ -310,59 +317,84 @@ def join(req: UserUpdateRequest, db: Session = Depends(get_db)):
     return {"message": "회원정보 수정이 완료되었습니다."}
     
 # Backend API: 동화책 상세정보 조회
-@app.get("/users/{uid}/detail", response_model=DetailResponse)
-def book_detail(
-    uid: int,
-    fid: int = Query(..., description="동화 ID"),
-    db: Session = Depends(get_db)
-):
-    row = (
-        db.query(FairyTale)
-        .filter(FairyTale.uid == uid, FairyTale.fid == fid)
-        .first()
-    )
+# @app.get("/users/{uid}/detail", response_model=DetailResponse)
+# def book_detail(
+#     uid: int,
+#     fid: int = Query(..., description="동화 ID"),
+#     db: Session = Depends(get_db)
+# ):
+#     row = (
+#         db.query(FairyTale)
+#         .filter(FairyTale.uid == uid, FairyTale.fid == fid)
+#         .first()
+#     )
 
-    if not row:
-        raise HTTPException(status_code=404, detail="해당 동화를 찾을 수 없음")
+#     if not row:
+#         raise HTTPException(status_code=404, detail="해당 동화를 찾을 수 없음")
 
-    return DetailResponse(
-        uid=row.uid,
-        type=row.type,
-        title=row.title,
-        summary=row.summary,
-        contents=row.contents,
-        create_dates=row.createDate,
-    )
+#     return DetailResponse(
+#         uid=row.uid,
+#         type=row.type,
+#         title=row.title,
+#         summary=row.summary,
+#         contents=row.contents,
+#         create_dates=row.createDate,
+#     )
 
 # Backend API: 동화책 검색
-@app.get("/users/{uid}/search", response_model=SearchResponse)
-def search_books(
-    uid: int = Query(None, description="사용자 ID"),
-    type: Optional[int] = Query(None, description="기록 타입"),
-    title: Optional[str] = Query(None, description="책 제목 (검색용)"),
-    db: Session = Depends(get_db)):
+# @app.get("/users/{uid}/search", response_model=SearchResponse)
+# def search_books(
+#     uid: int = Query(None, description="사용자 ID"),
+#     type: Optional[int] = Query(None, description="기록 타입"),
+#     title: Optional[str] = Query(None, description="책 제목 (검색용)"),
+#     db: Session = Depends(get_db)):
     
-    query = db.query(FairyTale)
+#     query = db.query(FairyTale)
     
-    # uid 필터 검색
-    if uid is not None:
-        query = query.filter(FairyTale.uid == uid)
+#     # uid 필터 검색
+#     if uid is not None:
+#         query = query.filter(FairyTale.uid == uid)
         
-        # uid가 있는 경우에만 type 필터 검색
-        if type is not None:
-            query = query.filter(FairyTale.type == type)
+#         # uid가 있는 경우에만 type 필터 검색
+#         if type is not None:
+#             query = query.filter(FairyTale.type == type)
     
-    # 제목 필터 검색
-    if title:
-        query = query.filter(FairyTale.title.contains(title))
+#     # 제목 필터 검색
+#     if title:
+#         query = query.filter(FairyTale.title.contains(title))
         
-    records = query.all()
+#     records = query.all()
     
-    return SearchResponse(
-        uid=uid,
-        type=[r.type for r in records],
-        title=[r.title for r in records],
-        summary=[r.summary for r in records],
-        contents=[r.contents for r in records],
-        create_dates=[r.createDate for r in records],
-    )
+#     return SearchResponse(
+#         uid=uid,
+#         type=[r.type for r in records],
+#         title=[r.title for r in records],
+#         summary=[r.summary for r in records],
+#         contents=[r.contents for r in records],
+#         create_dates=[r.createDate for r in records],
+#     )
+
+# 회원 탈죄 API
+@app.post("/delete_user", response_model=UserDeleteResponse)
+def delete_user(req: UserDeleteRequest,  db: Session = Depends(get_db)):
+    # 유저 조회
+    user = db.query(Users).filter(Users.uid == req.uid, Users.useFlag == 1).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="해당 사용자가 존재하지 않습니다.")
+
+    # 비밀번호 확인
+    if not pwd_context.verify(req.passwd, user.passwd):
+        raise HTTPException(status_code=400, detail="비밀번호가 일치하지 않습니다.")
+
+    # 탈퇴 처리 (soft delete)
+    user.useFlag = 0
+    user.updateDate = date.today()
+
+    try:
+        db.commit()
+        db.refresh(user)
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=500, detail="서버 내부에 오류가 발생했습니다.")
+
+    return {"message": "회원탈퇴가 완료되었습니다."}
